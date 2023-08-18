385152c5a48a8c48d2eb71d2941ef257288335da
Awais <awaisdev2@gmail.com>
2023-08-18 15:54:02 +0500

    create top navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  return <div className='bg-pink-200'>Header</div>;
+  return <header className='py-8'>
+   <div className="container mx-auto">
+   <div className="flex justify-between items-center">
+     <img src={Logo} alt="logo" />
+     <button className='btn btn-sm'>Work with me</button>
+   </div>
+   </div>
+    </header>;
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
   return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
     <div className="container mx-auto">
       <div className="w-full bg-black/20 backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50">
-        <Link activeClass='active' smooth={true} spy={true} to='home' className='flex justify-center cursor-pointer items-center w-[60px] h-[60px]'>
+        <Link activeClass='active' smooth={true} spy={true} offset={-200} to='home' className='flex justify-center cursor-pointer items-center w-[60px] h-[60px]'>
          <BiHomeAlt />
         </Link>
         <Link activeClass='active' smooth={true} spy={true} to='about' className='flex justify-center cursor-pointer items-center w-[60px] h-[60px]'>
