175cb34bda7ba584b2305536e43ee59f3818e0dc
Awais <awaisdev2@gmail.com>
2023-08-18 20:14:12 +0500

    create contact form

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React from "react";
+import Image from "../assets/avatar.svg";
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
 
 const Banner = () => {
-  return <div className='section' id='home'>Banner</div>;
+  return (
+    <motion.section
+      className="section"
+      id="home"
+      initial="initial"
+      animate="animate"
+      variants={fadeIn}
+    >
+      <div className="container mx-auto">
+        <motion.div
+          className="flex flex-col gap-y-8 lg:flex-row items-center"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 0.7 }}
+        >
+          <div className="flex-1 text-center lg:text-left">
+            <div className="text-4xl mb-3">
+              <h1 className="text-6xl font-bold">
+                Muhammad <span>Awais</span>
+              </h1>
+              <div>
+                <span className="mr-2">I'm a</span>
+                <TypeAnimation
+                  sequence={[
+                    "Developer",
+                    2000,
+                    "Designer",
+                    2000,
+                    "Youtuber",
+                    2000,
+                  ]}
+                  speed={50}
+                  className="text-accent"
+                  wrapper="span"
+                  repeat={Infinity}
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                atque labore eum non ratione ipsa.
+              </p>
+              <div className="my-5">
+                <Link smooth={true} to="contact" className="btn cursor-pointer px-5 py-4 btn-sm mr-3">Contact me</Link>
+              </div>
+              <div className="flex justify-center md:justify-center lg:justify-normal my-3">
+                <FaGithub className="mx-1 cursor-pointer" />
+                <FaYoutube className="mx-1 cursor-pointer" />
+                <FaDribbble className="mx-1 cursor-pointer" />
+              </div>
+            </div>
+          </div>
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.7 }}
+          >
+            <img src={Image} alt="avatar" />
+          </motion.div>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import Logo from '../assets/logo.svg'
+import { React } from "react";
+import { Link } from "react-scroll";
 
 const Header = () => {
-  return <header className='py-8'>
-   <div className="container mx-auto">
-   <div className="flex justify-between items-center">
-     <img src={Logo} alt="logo" />
-     <button className='btn btn-sm'>Work with me</button>
-   </div>
-   </div>
-    </header>;
+
+  return (
+    <header className="pt-4">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="uppercase font-bold m-0 text-2xl">
+              <span className="text-blue-300">Muha</span>
+              <span className="text-purple-500">mmad</span>
+            </p>
+            <p className="text-2xl font-bold text-slate-50 m-0">AWAIS</p>
+          </div>
+          <Link
+          smooth={true}
+          spy={true}
+            className="bg-blue-600 p-2 rounded-xl cursor-pointer"
+            to="work"
+          >
+            Work with me
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
